4ca328031b1528e0fb160ba7a8ada36a22dca174
Igor Matheus <mattigor.impr@gmail.com>
2025-11-25 18:12:49 -0300

    chore: update

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,8 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
+    @Get('list/id')
     findAll() {
         return "Listagem de cursos";
     }
 }
+
+// localhost:3000/courses/list
